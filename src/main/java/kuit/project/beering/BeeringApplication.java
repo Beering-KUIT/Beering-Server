2e5c0b0515029382703c4494944805786d52fee0
hhj <gudwn357@gmail.com>
2023-07-03 15:18:09 +0900

    fist commit

---
+++ src/main/java/kuit/project/beering/BeeringApplication.java
@@ -0,0 +1,13 @@
+package kuit.project.beering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeeringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeeringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kuit/project/beering/BeeringApplicationTests.java
@@ -0,0 +1,13 @@
+package kuit.project.beering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeeringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
