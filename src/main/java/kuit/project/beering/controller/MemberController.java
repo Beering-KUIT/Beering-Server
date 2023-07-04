a1482e85a17b44cfcd8693101b1136f47498c9ac
hhj <gudwn357@gmail.com>
2023-07-04 15:57:13 +0900

    feat (MemberController) : 로그인
    추가로 signup()에서 @GetMapping -> @PostMapping 으로 수정

--- src/main/java/kuit/project/beering/controller/MemberController.java
+++ src/main/java/kuit/project/beering/controller/MemberController.java
@@ -1,11 +1,14 @@
 package kuit.project.beering.controller;
 
+import kuit.project.beering.dto.request.MemberLoginRequest;
 import kuit.project.beering.dto.request.MemberSignupRequest;
+import kuit.project.beering.dto.response.MemberLoginResponse;
 import kuit.project.beering.service.MemberService;
 import kuit.project.beering.util.BaseResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,11 +19,19 @@ public class MemberController {
 
     private final MemberService memberService;
 
-    @GetMapping("/signup")
+    @PostMapping("/signup")
     public BaseResponse<Object> signup(@RequestBody MemberSignupRequest request) {
 
         memberService.signup(request);
 
         return new BaseResponse<>(new Object());
+    }
+
+    @PostMapping("/login")
+    public BaseResponse<MemberLoginResponse> login(@RequestBody MemberLoginRequest request) {
+
+        MemberLoginResponse response = memberService.login(request);
+
+        return new BaseResponse<>(response);
     }
 }
