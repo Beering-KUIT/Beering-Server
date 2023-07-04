4b71bf993ccc8546ce341032409dbd25fb081ddf
hhj <gudwn357@gmail.com>
2023-07-04 13:25:42 +0900

    feat (AuthMember) : create
    인증 정보를 담은 객체. Spring Security Context에 올라감

---
+++ src/main/java/kuit/project/beering/security/auth/AuthMember.java
@@ -0,0 +1,55 @@
+package kuit.project.beering.security.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Getter
+@AllArgsConstructor
+@Builder
+public class AuthMember implements UserDetails {
+
+    private Long id;
+    private String username;
+    private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return new ArrayList<>();
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
