d2dc761c98cbd6c37101009e873bbebe2c86654d
hhj <gudwn357@gmail.com>
2023-07-04 16:06:53 +0900

    feat (MemberLoginRequest) : create
    로그인 request dto

---
+++ src/main/java/kuit/project/beering/dto/request/MemberLoginRequest.java
@@ -0,0 +1,15 @@
+package kuit.project.beering.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MemberLoginRequest {
+    private String username;
+    private String password;
+}
