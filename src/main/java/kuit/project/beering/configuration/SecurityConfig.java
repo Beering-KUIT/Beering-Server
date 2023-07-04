d9efe198b452d1754574dc1689f5bf53bb8c1d72
hhj <gudwn357@gmail.com>
2023-07-04 11:25:49 +0900

    feat (SecurityConfig) : 스프링 시큐리티 관련 설정파일 추가

---
+++ src/main/java/kuit/project/beering/configuration/SecurityConfig.java
@@ -0,0 +1,31 @@
+package kuit.project.beering.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .sessionManagement(sessionManagement ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorizeRequest -> {
+                    authorizeRequest.requestMatchers("/").permitAll();
+                });
+
+
+        return http.build();
+    }
+}
