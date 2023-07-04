b55cebc7f0e44f1db264748264b2a10f59f23e93
hhj <gudwn357@gmail.com>
2023-07-04 11:27:32 +0900

    feat (SecurityConfig) : add bean BCryptPasswordEncoder

--- src/main/java/kuit/project/beering/configuration/SecurityConfig.java
+++ src/main/java/kuit/project/beering/configuration/SecurityConfig.java
@@ -6,11 +6,18 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
