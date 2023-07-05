6f69c7cf313948f8e29667aa1e8e608497e9c237
JooeonLee <cacophony0609@gmail.com>
2023-07-05 15:13:10 +0900

    add : create Category domain
    Category 도메인 생성

---
+++ src/main/java/kuit/project/beering/domain/Category.java
@@ -0,0 +1,39 @@
+package kuit.project.beering.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Category extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column
+    private Status status;
+
+    //연관관계 mapping
+    @OneToMany(mappedBy = "category")
+    private List<Beer> beers = new ArrayList<>();
+
+    @OneToMany(mappedBy = "category")
+    private List<ReviewOption> reviewOptions = new ArrayList<>();
+
+    @OneToMany(mappedBy = "category")
+    private List<Review> reviews = new ArrayList<>();
+}
