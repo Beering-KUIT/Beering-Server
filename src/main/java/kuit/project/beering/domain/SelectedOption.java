0b03eb2c75a4661f64fb3a97229dff16ac5d9a0d
JooeonLee <cacophony0609@gmail.com>
2023-07-05 15:18:38 +0900

    add : create SelectedOption domain
    SelectedOption 도메인 생성

---
+++ src/main/java/kuit/project/beering/domain/SelectedOption.java
@@ -0,0 +1,35 @@
+package kuit.project.beering.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class SelectedOption extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "selected_option_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private float rating;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Status status;
+
+    //연관관계 mapping
+    @OneToOne
+    @JoinColumn(name = "review_option_id")
+    private ReviewOption reviewOption;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "review_id")
+    private Review review;
+}
