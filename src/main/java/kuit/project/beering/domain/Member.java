1342e9cad19794f993cfd5110bc3a4e54651ff54
JooeonLee <cacophony0609@gmail.com>
2023-07-05 15:22:38 +0900

    update : add field in Member domain
    Member 도메인에 필드 추가

--- src/main/java/kuit/project/beering/domain/Member.java
+++ src/main/java/kuit/project/beering/domain/Member.java
@@ -3,12 +3,15 @@ package kuit.project.beering.domain;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Builder
-public class Member {
+public class Member extends BaseTimeEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY) @Column(name = "member_id", nullable = false)
     private Long id;
@@ -20,4 +23,21 @@ public class Member {
     @Column(nullable = false)
     private String nickname;
 
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Status status;
+
+    //연관관계 mapping
+    @OneToOne
+    @JoinColumn(name = "image_id")
+    private Image image;
+
+    @OneToMany(mappedBy = "member")
+    private List<AgreementMember> agreementMembers = new ArrayList<>();
+
+    @OneToMany(mappedBy = "member")
+    private List<Favorite> favorites = new ArrayList<>();
+
+    @OneToMany(mappedBy = "member")
+    private List<Review> reviews = new ArrayList<>();
 }
