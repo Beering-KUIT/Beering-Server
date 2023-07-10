397f92d0e9653dc2f48d3ced842d55da9f5a31fe
hhj <gudwn357@gmail.com>
2023-07-10 17:56:40 +0900

    refactor (AgreementRequest) : change field
    id 대신 name을 받도록 변경

--- src/main/java/kuit/project/beering/dto/request/member/AgreementRequest.java
+++ src/main/java/kuit/project/beering/dto/request/member/AgreementRequest.java
@@ -2,13 +2,15 @@ package kuit.project.beering.dto.request.member;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class AgreementRequest {
 
-    private Long id;
+    private String name;
     private Boolean isAgreed;
 }
