5c067741f9c1b2fa4ad4918e50d18d6d63546d88
hhj <gudwn357@gmail.com>
2023-07-11 17:26:51 +0900

    feat (Member) : add field Role
    status, role 값 생성 메서드에 추가

--- src/main/java/kuit/project/beering/domain/Member.java
+++ src/main/java/kuit/project/beering/domain/Member.java
@@ -25,6 +25,10 @@ public class Member extends BaseTimeEntity {
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
+    private Role role;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private Status status;
 
     //연관관계 mapping
@@ -50,6 +54,8 @@ public class Member extends BaseTimeEntity {
         member.password = password;
         member.nickname = nickname;
 
+        member.status = Status.ACTIVE;
+        member.role = Role.MEMBER;
         return member;
     }
 }
