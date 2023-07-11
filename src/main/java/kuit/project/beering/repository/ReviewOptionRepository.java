677c9005ea1154d34dbb3e503be4c505c363f8f8
JooeonLee <cacophony0609@gmail.com>
2023-07-11 17:28:17 +0900

    feat (ReviewOptionRepository) : add find logic by categoryId

--- src/main/java/kuit/project/beering/repository/ReviewOptionRepository.java
+++ src/main/java/kuit/project/beering/repository/ReviewOptionRepository.java
@@ -1,4 +1,17 @@
 package kuit.project.beering.repository;
 
-public interface ReviewOptionRepository {
+import kuit.project.beering.domain.ReviewOption;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface ReviewOptionRepository extends JpaRepository<ReviewOption, Long> {
+
+    @Query("select ro from ReviewOption ro " +
+            "join fetch ro.category " +
+            "where ro.category.id = :categoryId")
+    List<ReviewOption> findAllFetchByCategoryId(Long categoryId);
+
+    List<ReviewOption> findAllByCategoryId(Long categoryId);
 }
