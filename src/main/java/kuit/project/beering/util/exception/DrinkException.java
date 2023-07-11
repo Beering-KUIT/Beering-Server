ef6cabaa283fa2cec451e9b55c1d58fe83c5de26
JooeonLee <cacophony0609@gmail.com>
2023-07-11 17:23:17 +0900

    feat (DrinkException) : add DrinkException

--- src/main/java/kuit/project/beering/util/exception/DrinkException.java
+++ src/main/java/kuit/project/beering/util/exception/DrinkException.java
@@ -1,4 +1,13 @@
 package kuit.project.beering.util.exception;
 
-public class DrinkException {
+import kuit.project.beering.util.BaseResponseStatus;
+import lombok.Getter;
+
+@Getter
+public class DrinkException extends RuntimeException {
+    private final BaseResponseStatus status;
+
+    public DrinkException(BaseResponseStatus status) {
+        this.status = status;
+    }
 }
