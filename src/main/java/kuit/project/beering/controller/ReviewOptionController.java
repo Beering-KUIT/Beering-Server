9958c5a96ce83da7fa0783152c0d261a47e42588
JooeonLee <cacophony0609@gmail.com>
2023-07-11 17:46:19 +0900

    feat (ReviewOptionController) : add review option read api
    컨트롤러에 리뷰옵션 GET하는 API 추가

--- src/main/java/kuit/project/beering/controller/ReviewOptionController.java
+++ src/main/java/kuit/project/beering/controller/ReviewOptionController.java
@@ -1,4 +1,28 @@
 package kuit.project.beering.controller;
 
+import kuit.project.beering.dto.response.reviewOption.ReviewOptionReadResponseDto;
+import kuit.project.beering.service.ReviewOptionService;
+import kuit.project.beering.util.BaseResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
 public class ReviewOptionController {
+
+    private final ReviewOptionService reviewOptionService;
+
+    @GetMapping("/reviewOptions")
+    public BaseResponse<List<ReviewOptionReadResponseDto>> readAllReviewOptionByDrinkId(
+            @RequestParam(name = "drinkId")Long drinkId) {
+
+        List<ReviewOptionReadResponseDto> responseDtos = reviewOptionService.findAllReviewOptionByDrinkId(drinkId);
+        return new BaseResponse<>(responseDtos);
+    }
 }
