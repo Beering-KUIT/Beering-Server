c1be6cd12eed6dfa4352ff34463410fd5e5ba5b9
hhj <gudwn357@gmail.com>
2023-07-11 16:02:03 +0900

    add (AgreementJdbcRepository) :

---
+++ src/main/java/kuit/project/beering/repository/AgreementJdbcRepository.java
@@ -0,0 +1,10 @@
+package kuit.project.beering.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class AgreementJdbcRepository {
+
+}
