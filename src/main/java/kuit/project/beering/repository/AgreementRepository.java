a1f5c4801b32bad0b408e7d24c45181234ad0ee4
hhj <gudwn357@gmail.com>
2023-07-11 17:37:49 +0900

    Revert "add (AgreementRepository) : create JpaRepository"
    This reverts commit c324dcd72467ac19d1902428134a24bd7832f038.

--- src/main/java/kuit/project/beering/repository/AgreementRepository.java
+++
@@ -1,7 +0,0 @@
-package kuit.project.beering.repository;
-
-import kuit.project.beering.domain.Agreement;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AgreementRepository extends JpaRepository<Agreement, Long> {
-}
