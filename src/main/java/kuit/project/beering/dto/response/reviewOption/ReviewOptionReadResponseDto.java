84be3806947a068dcd89d5152707caf0fd8c4e8a
JooeonLee <cacophony0609@gmail.com>
2023-07-11 17:25:25 +0900

    feat (ReviewOptionReadResponseDto) : add response dto

--- src/main/java/kuit/project/beering/dto/response/reviewOption/ReviewOptionReadResponseDto.java
+++ src/main/java/kuit/project/beering/dto/response/reviewOption/ReviewOptionReadResponseDto.java
@@ -1,4 +1,20 @@
 package kuit.project.beering.dto.response.reviewOption;
 
+import kuit.project.beering.domain.ReviewOption;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@Builder
 public class ReviewOptionReadResponseDto {
+
+    private Long reviewOptionId;
+    private String name;
+
+    public ReviewOptionReadResponseDto(ReviewOption reviewOption) {
+        this.reviewOptionId = reviewOption.getId();
+        this.name = reviewOption.getName();
+    }
 }
