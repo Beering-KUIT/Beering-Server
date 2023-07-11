e06fa173293656ad3c0968058225daef0f4b0cc6
hhj <gudwn357@gmail.com>
2023-07-11 16:04:52 +0900

    feat (AgreementBulkInsertDto) : create for jdbc bulk query

---
+++ src/main/java/kuit/project/beering/dto/AgreementBulkInsertDto.java
@@ -0,0 +1,15 @@
+package kuit.project.beering.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@Builder
+public class AgreementBulkInsertDto {
+
+    private String name;
+    private Boolean isAgreed;
+    private Long memberId;
+}
