2be38780d7ff203d7fb940cf0a7b4813a61b7ab4
kwiyoon <jiyun0245@gmail.com>
2023-07-13 03:49:33 +0900

    feat (FavoriteRepository) : impl favorite check

---
+++ src/main/java/kuit/project/beering/repository/FavoriteRepository.java
@@ -0,0 +1,8 @@
+package kuit.project.beering.repository;
+
+import kuit.project.beering.domain.Favorite;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
+    boolean existsByDrinkIdAndMemberId(Long drinkId, Long userId);
+}
