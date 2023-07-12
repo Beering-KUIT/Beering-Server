56dfc346d78c469bbcc515f799cd35a12eb3f6b8
kwiyoon <jiyun0245@gmail.com>
2023-07-13 03:06:56 +0900

    feat (DrinkController) : implement search drink
    주류 검색의 기본적인 기능 구현

--- src/main/java/kuit/project/beering/controller/DrinkController.java
+++ src/main/java/kuit/project/beering/controller/DrinkController.java
@@ -1,8 +1,12 @@
 package kuit.project.beering.controller;
 
+import kuit.project.beering.dto.response.drink.DrinkSearchResponse;
+import kuit.project.beering.util.BaseResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
+import kuit.project.beering.service.DrinkService;
 
 
 @RestController
@@ -10,5 +14,25 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 @RequestMapping("/drinks")
 public class DrinkController {
+
+    private final DrinkService drinkService;
+
+    @GetMapping("/search")
+    public BaseResponse<Page<DrinkSearchResponse>> searchDrinksByName(
+            @RequestParam(required = false) String name,
+            @RequestParam(defaultValue = "0", required = false) Integer page,
+            @RequestParam(defaultValue = "name", required = false) String orderBy,
+            @RequestParam(required = false) String category,
+            @RequestParam(defaultValue = "0", required = false) Integer minPrice,
+            @RequestParam(required = false) Integer maxPrice
+    ) {
+        if (maxPrice == null) {
+            maxPrice = Integer.MAX_VALUE;
+        }
+
+        Page<DrinkSearchResponse> result = drinkService.searchDrinksByName(name, orderBy, page);
+        return new BaseResponse<>(result);
+    }
+
 
 }
