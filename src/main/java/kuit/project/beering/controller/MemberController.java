aaced2765d1caea46fc0c193619f3e7026607725
hhj <gudwn357@gmail.com>
2023-07-12 17:22:49 +0900

    feat (MemberController) : valid request
    회원 정보 및 약관 정보 검증

--- src/main/java/kuit/project/beering/controller/MemberController.java
+++ src/main/java/kuit/project/beering/controller/MemberController.java
@@ -1,16 +1,24 @@
 package kuit.project.beering.controller;
 
+import jakarta.validation.Valid;
+import kuit.project.beering.domain.AgreementName;
+import kuit.project.beering.dto.request.member.AgreementRequest;
 import kuit.project.beering.dto.request.member.MemberLoginRequest;
 import kuit.project.beering.dto.request.member.MemberSignupRequest;
 import kuit.project.beering.dto.response.member.MemberLoginResponse;
 import kuit.project.beering.service.MemberService;
 import kuit.project.beering.util.BaseResponse;
+import kuit.project.beering.util.UserValidationException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,7 +28,12 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping("/signup")
-    public BaseResponse<Object> signup(@RequestBody MemberSignupRequest request) {
+    public BaseResponse<Object> signup(@RequestBody @Valid MemberSignupRequest request,
+                                       BindingResult bindingResult) {
+
+        validateAgreementName(request, bindingResult);
+
+        if (bindingResult.hasErrors()) throw new UserValidationException(bindingResult);
 
         memberService.signup(request);
 
@@ -33,5 +46,24 @@ public class MemberController {
         MemberLoginResponse response = memberService.login(request);
 
         return new BaseResponse<>(response);
+    }
+
+    /**
+     * @Brief Agreement 값 제대로 들어왔는지 확인. 개수, 약관 포함 여부
+     * @param memberSignupRequest
+     * @param bindingResult
+     */
+    private static void validateAgreementName(MemberSignupRequest memberSignupRequest, BindingResult bindingResult) {
+        List<AgreementName> agreementNames = memberSignupRequest.getAgreements().stream()
+                .map(AgreementRequest::getName).toList();
+
+        /**
+         * @Condition 약관 개수와 필요한 약관이 모두 들어왔는지
+         */
+        if (agreementNames.size() != AgreementName.values().length &&
+                !agreementNames.containsAll(Arrays.stream(AgreementName.values()).toList())) {
+            bindingResult.addError(new ObjectError
+                    ("Agreement", "SERVICE, PERSONAL, MARKETING 을 모두 포함"));
+        }
     }
 }
