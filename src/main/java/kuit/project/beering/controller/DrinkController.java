4b889d81965e496a985d8e90ef2e24ad2d02a4be
kwiyoon <jiyun0245@gmail.com>
2023-07-13 02:55:33 +0900

    create (DrinkController) : Add mapping for '/drinks' api

---
+++ src/main/java/kuit/project/beering/controller/DrinkController.java
@@ -0,0 +1,14 @@
+package kuit.project.beering.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+@RequestMapping("/drinks")
+public class DrinkController {
+
+}
