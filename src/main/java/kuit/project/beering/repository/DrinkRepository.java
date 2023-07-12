b5242f44e08d77794fc5012cba9ab554d06c9dd5
kwiyoon <jiyun0245@gmail.com>
2023-07-13 03:25:10 +0900

    feat : implement findByName with paging

--- src/main/java/kuit/project/beering/repository/DrinkRepository.java
+++ src/main/java/kuit/project/beering/repository/DrinkRepository.java
@@ -1,4 +1,11 @@
 package kuit.project.beering.repository;
 
-public interface DrinkRepository{
+import kuit.project.beering.domain.Drink;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DrinkRepository extends JpaRepository<Drink, Long> {
+    /* 주류 가져오기 - 이름 */
+    Page<Drink> findByNameKrContainingOrNameEnContainingIgnoreCase(String nameKr, String nameEn, Pageable pageable);
 }
