669b706366425d23bb5efe7a67960115f887c7f7
hhj <gudwn357@gmail.com>
2023-07-12 17:23:37 +0900

    feat (UserValidationException) : create exception for validation

---
+++ src/main/java/kuit/project/beering/util/UserValidationException.java
@@ -0,0 +1,16 @@
+package kuit.project.beering.util;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.validation.BindingResult;
+
+/**
+ * @Brief 회원 가입 검증 예외
+ */
+@Getter
+@AllArgsConstructor
+public class UserValidationException extends RuntimeException {
+
+    private BindingResult bindingResult;
+
+}
