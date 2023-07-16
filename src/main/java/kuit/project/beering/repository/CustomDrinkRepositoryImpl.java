9b0958c633c3ae8c40ebd3974f833c22d522b8b7
kwiyoon <jiyun0245@gmail.com>
2023-07-16 20:41:10 +0900

    feat : implement search in CustomDrinkRepo
    - CustomDrinkRepositoryImpl 의 search() 구현
    - BooleanExpression을 이용하여 검색 조건의 유무에 따른 쿼리 처리
    - 페이징 처리

--- src/main/java/kuit/project/beering/repository/CustomDrinkRepositoryImpl.java
+++ src/main/java/kuit/project/beering/repository/CustomDrinkRepositoryImpl.java
@@ -1,11 +1,18 @@
 package kuit.project.beering.repository;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import kuit.project.beering.domain.Drink;
 import kuit.project.beering.dto.request.drink.DrinkSearchCondition;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
+import org.springframework.util.StringUtils;
+import java.util.List;
+
+import static kuit.project.beering.domain.QDrink.*;
 
 public class CustomDrinkRepositoryImpl implements CustomDrinkRepository {
     private final JPAQueryFactory jpaQueryFactory;
@@ -14,8 +21,37 @@ public class CustomDrinkRepositoryImpl implements CustomDrinkRepository {
         this.jpaQueryFactory = new JPAQueryFactory(em);
     }
 
-    @Override
-    public Page<Drink> search(DrinkSearchCondition drinkSearchCondition, Pageable pageable) {
-        return null;
+    public Page<Drink> search(DrinkSearchCondition condition, Pageable pageable){
+        List<Drink> content = jpaQueryFactory
+                                .selectFrom(drink)
+                                .where(eqName(condition.getNameKr(), condition.getNameEn()),
+                                        drink.price.between(condition.getMinPrice(), condition.getMaxPrice()),
+                                        eqCategory(condition.getCategoryName()))
+                                .offset(pageable.getOffset())
+                                .limit(pageable.getPageSize())
+                                .fetch();
+
+        JPAQuery<Long> countQuery = jpaQueryFactory
+                                    .select(drink.count())
+                                    .from(drink)
+                                    .where(eqName(condition.getNameKr(), condition.getNameEn()),
+                                            drink.price.between(condition.getMinPrice(), condition.getMaxPrice()),
+                                            eqCategory(condition.getCategoryName()));
+
+        return PageableExecutionUtils.getPage(content, pageable, countQuery::fetchOne);
+
     }
+
+    private BooleanExpression eqCategory(String categoryName) {
+        if(!StringUtils.hasText(categoryName))
+            return null;
+        return drink.category.name.eq(categoryName);
+    }
+
+    public BooleanExpression eqName(String nameKr, String nameEn){
+        if (!StringUtils.hasText(nameKr))
+            return null;
+        return drink.nameKr.eq(nameKr).or(drink.nameEn.eq(nameEn));
+    }
+
 }
