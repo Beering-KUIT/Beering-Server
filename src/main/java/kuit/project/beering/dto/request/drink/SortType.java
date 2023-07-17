15fc96d3b214ea558c68b34ce7b7c0ef47e71e8a
kwiyoon <jiyun0245@gmail.com>
2023-07-17 19:50:26 +0900

    feat (SortType) : implement getMatchedSort()
    string 형태로 정렬 방식을 입력받으면, 해당하는 Sort 객체를 반환한다.

--- src/main/java/kuit/project/beering/dto/request/drink/SortType.java
+++ src/main/java/kuit/project/beering/dto/request/drink/SortType.java
@@ -2,10 +2,29 @@ package kuit.project.beering.dto.request.drink;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+
+import java.util.Arrays;
+import java.util.Locale;
 
 
 @Getter
 @RequiredArgsConstructor
 public enum SortType {
+    REVIEW(Sort.by(Direction.DESC, "countOfReview")),
+    RATING(Sort.by(Direction.DESC, "avgRating")),
+    PRICE(Sort.by(Direction.ASC, "price")),
+    NAME(Sort.by(Direction.ASC, "nameKr")),
+    OTHER(Sort.by(Direction.ASC, "id"));
 
+    private final Sort sort;
+
+    public static Sort getMatchedSort(String sorting) {
+        return Arrays.stream(values())
+                .filter(sortType -> sortType.name().equals(sorting.toUpperCase(Locale.ROOT)))
+                .findAny()
+                .orElse(OTHER)
+                .sort;
+    }
 }
