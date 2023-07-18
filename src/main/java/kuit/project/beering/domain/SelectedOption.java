504bca9e18bedd52e4697b1372df31a49a7e18a7
JooeonLee <cacophony0609@gmail.com>
2023-07-18 16:36:20 +0900

    feat (SelectedOption) : add constructor
    SelectedOption domain 생성자 추가

--- src/main/java/kuit/project/beering/domain/SelectedOption.java
+++ src/main/java/kuit/project/beering/domain/SelectedOption.java
@@ -1,10 +1,7 @@
 package kuit.project.beering.domain;
 
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
 @Getter
@@ -32,4 +29,22 @@ public class SelectedOption extends BaseTimeEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "review_id")
     private Review review;
+
+    //리뷰 엔티티에 빌더 추가
+    @Builder
+    public SelectedOption(ReviewOption reviewOption, Review review, float rating, Status status) {
+
+        this.reviewOption = reviewOption;
+        if (reviewOption != null) {
+            reviewOption.addSelectedOption(this);
+        }
+
+        this.review = review;
+        if (review != null) {
+            review.addSelectedOption(this);
+        }
+
+        this.rating = rating;
+        this.status = status;
+    }
 }
