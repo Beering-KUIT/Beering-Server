ee6a9c53b5512e91fa3a94785e30ee7d2bef6f27
kwiyoon <jiyun0245@gmail.com>
2023-07-18 14:41:29 +0900

    fix : handle all exceptions
    in DrinkControllerAdvice
    - 실제 사용 예시
       new DrinkException(INVALID_ORDER)

--- src/main/java/kuit/project/beering/controller/advice/DrinkControllerAdvice.java
+++ src/main/java/kuit/project/beering/controller/advice/DrinkControllerAdvice.java
@@ -2,7 +2,6 @@ package kuit.project.beering.controller.advice;
 
 import kuit.project.beering.controller.DrinkController;
 import kuit.project.beering.util.BaseResponse;
-import kuit.project.beering.util.BaseResponseStatus;
 import kuit.project.beering.util.exception.DrinkException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -10,7 +9,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice(assignableTypes = DrinkController.class)
 public class DrinkControllerAdvice {
     @ExceptionHandler(DrinkException.class)
-    public BaseResponse<Object> invalidOrder(DrinkException ex) {
-        return new BaseResponse<>(BaseResponseStatus.INVALID_ORDER);
+    public BaseResponse<Object> handleDrinkException(DrinkException ex) {
+        return new BaseResponse<>(ex.getStatus());
     }
 }
