0dcc4193a097ef15fc8023b1a7c6fa7b72b6d78a
hhj <gudwn357@gmail.com>
2023-07-19 15:38:56 +0900

    refactor (MemberController) : edit mapping url

--- src/main/java/kuit/project/beering/controller/MemberController.java
+++ src/main/java/kuit/project/beering/controller/MemberController.java
@@ -17,6 +17,7 @@ import org.springframework.validation.ObjectError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
@@ -25,6 +26,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @Slf4j
+@RequestMapping("/auth")
 public class MemberController {
 
     private final MemberService memberService;
