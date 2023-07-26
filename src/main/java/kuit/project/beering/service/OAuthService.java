8ba65b2f98d7277e36aa0a6a3032fca62bd79103
hhj <gudwn357@gmail.com>
2023-07-26 12:41:14 +0900

    feat (OAuthService) : create get token request

--- src/main/java/kuit/project/beering/service/OAuthService.java
+++ src/main/java/kuit/project/beering/service/OAuthService.java
@@ -1,5 +1,68 @@
 package kuit.project.beering.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import kuit.project.beering.dto.response.member.MemberLoginResponse;
+import kuit.project.beering.security.jwt.OAuthTokenInfo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class OAuthService {
+
+    @Value(value = "${oauth2-kakao-restapi-key}")
+    private String CLIENT_ID;
+
+    @Transactional
+    public MemberLoginResponse kakaoOAuth(String code) throws JsonProcessingException {
+        // 1. "인가 코드"로 "액세스 토큰" 요청
+        OAuthTokenInfo oAuthTokenInfo = getAccessToken(code);
+
+    }
+
+    private OAuthTokenInfo getAccessToken(String code) throws JsonProcessingException {
+        // HTTP Header 생성
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_TYPE, "application/x-www-form-urlencoded;charset=utf-8");
+
+        // HTTP Body 생성
+        // TODO client_secret 고려해보기
+        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+        body.add("grant_type", "authorization_code");
+        body.add("client_id", CLIENT_ID);
+        body.add("redirect_uri", "http://localhost:9000/auth2/login");
+        body.add("code", code);
+
+        // HTTP 요청 보내기
+        HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest = new HttpEntity<>(body, headers);
+        RestTemplate rt = new RestTemplate();
+        ResponseEntity<String> response = rt.exchange(
+                "https://kauth.kakao.com/oauth/token",
+                HttpMethod.POST,
+                kakaoTokenRequest,
+                String.class
+        );
+
+        // HTTP 응답 (JSON) -> 액세스 토큰 파싱
+        String responseBody = response.getBody();
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode jsonNode = objectMapper.readTree(responseBody);
+        return OAuthTokenInfo.builder()
+                .accessToken(jsonNode.get("access_token").asText())
+                .refreshToken(jsonNode.get("refresh_token").asText()).build();
+
+    }
 
 }
