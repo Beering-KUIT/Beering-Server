cb23cfd24860b4b442e7e89c52f3e5080fa4f43e
JooeonLee <cacophony0609@gmail.com>
2023-07-27 17:13:56 +0900

    feat (ReviewImage) : add constructor

--- src/main/java/kuit/project/beering/domain/image/ReviewImage.java
+++ src/main/java/kuit/project/beering/domain/image/ReviewImage.java
@@ -3,10 +3,14 @@ package kuit.project.beering.domain.image;
 import jakarta.persistence.*;
 import kuit.project.beering.domain.Review;
 import kuit.project.beering.domain.image.Image;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.NoArgsConstructor;
 
 @Entity
 @DiscriminatorValue("review")
+@NoArgsConstructor
+@AllArgsConstructor
 public class ReviewImage extends Image {
 
     @ManyToOne(fetch = FetchType.LAZY)
