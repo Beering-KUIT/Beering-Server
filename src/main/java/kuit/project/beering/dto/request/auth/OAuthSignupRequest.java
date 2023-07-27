0cab87e7db69ce9e761d045d02b328923b20dfda
hhj <gudwn357@gmail.com>
2023-07-27 14:11:20 +0900

    add (OAuthSignupRequest)

---
+++ src/main/java/kuit/project/beering/dto/request/auth/OAuthSignupRequest.java
@@ -0,0 +1,29 @@
+package kuit.project.beering.dto.request.auth;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import kuit.project.beering.dto.request.member.AgreementRequest;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class OAuthSignupRequest {
+
+    @Pattern(regexp = "[a-zA-Z가-힣]+", message = "영문 또는 한글 1 ~ 10자리 이내")
+    @Length(min = 1, max = 10, message = "영문 또는 한글 1 ~ 10자리 이내")
+    private String nickname;
+
+    @NotBlank
+    private String sub;
+
+    private List<AgreementRequest> agreements = new ArrayList<>();
+}
