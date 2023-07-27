d338925aee41ca9b2668973ef9c3425573e73e99
hhj <gudwn357@gmail.com>
2023-07-27 16:09:43 +0900

    feat (OAuthService) : signup
    약관, 닉네임 받아서 oauth 회원 가입 처리 및 토큰 발행

--- src/main/java/kuit/project/beering/service/OAuthService.java
+++ src/main/java/kuit/project/beering/service/OAuthService.java
@@ -3,16 +3,18 @@ package kuit.project.beering.service;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.persistence.EntityNotFoundException;
 import kuit.project.beering.domain.Member;
 import kuit.project.beering.domain.OAuth;
 import kuit.project.beering.domain.OAuthType;
 import kuit.project.beering.dto.KakaoMemberInfo;
+import kuit.project.beering.dto.request.auth.OAuthSignupRequest;
+import kuit.project.beering.dto.request.member.MemberSignupRequest;
 import kuit.project.beering.dto.response.member.MemberLoginResponse;
 import kuit.project.beering.redis.RefreshToken;
 import kuit.project.beering.repository.MemberRepository;
 import kuit.project.beering.repository.OAuthRepository;
 import kuit.project.beering.repository.RefreshTokenRepository;
-import kuit.project.beering.security.auth.AuthMember;
 import kuit.project.beering.security.jwt.JwtInfo;
 import kuit.project.beering.security.jwt.JwtTokenProvider;
 import kuit.project.beering.security.jwt.OAuthTokenInfo;
@@ -23,20 +25,11 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -45,6 +38,7 @@ public class OAuthService {
 
     @Value(value = "${oauth2-kakao-restapi-key}")
     private String CLIENT_ID;
+    private final MemberService memberService;
     private final MemberRepository memberRepository;
     private final OAuthRepository oAuthRepository;
     private final RefreshTokenRepository refreshTokenRepository;
@@ -67,6 +61,38 @@ public class OAuthService {
                         .accessToken(oAuthTokenInfo.getIdToken())
                         .refreshToken(oAuthTokenInfo.getRefreshToken())
                         .build())
+                .build();
+    }
+
+    @Transactional
+    public MemberLoginResponse signup(OAuthSignupRequest request) throws JsonProcessingException {
+
+        OAuth oAuth = oAuthRepository.findBySub(request.getSub())
+                .orElseThrow(EntityNotFoundException::new);
+
+        JsonNode kakaoAccountResponseBody = getKakaoAccount(oAuth.getAccessToken());
+
+        String email = kakaoAccountResponseBody.get("kakao_account").get("email").asText();
+        String nickname = kakaoAccountResponseBody.get("properties")
+                .get("nickname").asText();
+
+        memberService.signup(new MemberSignupRequest(email, "!oauthpassword321", nickname, request.getAgreements()));
+        Member member = memberRepository.findByUsername(email).orElseThrow(EntityNotFoundException::new);
+        member.createOAuthAssociation(oAuth);
+
+        JsonNode tokenResponseBody = getTokenReissue(oAuth);
+
+        String idToken = tokenResponseBody.get("id_token").asText();
+        String accessToken = tokenResponseBody.get("access_token").asText();
+        String refreshToken = tokenResponseBody.get("refresh_token").asText();
+
+        oAuth.tokenReissue(accessToken, refreshToken);
+
+        refreshTokenRepository.save(new RefreshToken(String.valueOf(member.getId()), refreshToken));
+
+        return MemberLoginResponse.builder()
+                .memberId(member.getId())
+                .jwtInfo(JwtInfo.builder().accessToken(idToken).refreshToken(refreshToken).build())
                 .build();
     }
 
@@ -94,9 +120,7 @@ public class OAuthService {
         );
 
         // HTTP 응답 (JSON) -> 액세스 토큰 파싱
-        String responseBody = response.getBody();
-        ObjectMapper objectMapper = new ObjectMapper();
-        JsonNode jsonNode = objectMapper.readTree(responseBody);
+        JsonNode jsonNode = getResponseBody(response);
         return OAuthTokenInfo.builder()
                 .accessToken(jsonNode.get("access_token").asText())
                 .refreshToken(jsonNode.get("refresh_token").asText()).build();
@@ -105,9 +129,37 @@ public class OAuthService {
 
     private KakaoMemberInfo getKakaoUserInfo(String accessToken) throws JsonProcessingException {
         // HTTP Header 생성
+        JsonNode kakaoAccountResponseBody = getKakaoAccount(accessToken);
+
+        Long id = kakaoAccountResponseBody.get("id").asLong();
+        String email = kakaoAccountResponseBody.get("kakao_account").get("email").asText();
+        String nickname = kakaoAccountResponseBody.get("properties")
+                .get("nickname").asText();
+
+        return new KakaoMemberInfo(id, email, nickname);
+    }
+
+    // 3. 카카오ID로 회원가입 처리
+    private Member registerKakaoUserIfNeed(KakaoMemberInfo kakaoMemberInfo, OAuthTokenInfo oAuthTokenInfo) {
+        // DB 에 중복된 email이 있는지 확인
+        // 없으면 닉네임 및 약관 요청 있으면 강제 로그인..
+        Member member = memberRepository.findByUsername(kakaoMemberInfo.getEmail())
+                .orElseThrow(() -> {
+                    OAuth oAuth = oAuthRepository.save(OAuth.createOAuth(jwtTokenProvider.parseSub(oAuthTokenInfo.getIdToken()),
+                            OAuthType.KAKAO, oAuthTokenInfo.getAccessToken(), oAuthTokenInfo.getRefreshToken()));
+                    throw new SignupNotCompletedException(oAuth.getSub());
+                });
+
+        refreshTokenRepository.save(new RefreshToken(String.valueOf(member.getId()), oAuthTokenInfo.getRefreshToken()));
+
+        return member;
+    }
+
+    private JsonNode getKakaoAccount(String accessToken) throws JsonProcessingException {
         HttpHeaders headers = new HttpHeaders();
-        headers.add("Authorization", "Bearer " + accessToken);
-        headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+
+        headers.add(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken);
+        headers.add(HttpHeaders.CONTENT_TYPE, "application/x-www-form-urlencoded;charset=utf-8");
 
         // HTTP 요청 보내기
         HttpEntity<MultiValueMap<String, String>> kakaoUserInfoRequest = new HttpEntity<>(headers);
@@ -119,56 +171,34 @@ public class OAuthService {
                 String.class
         );
 
-        // responseBody에 있는 정보를 꺼냄
-        String responseBody = response.getBody();
-        ObjectMapper objectMapper = new ObjectMapper();
-        JsonNode jsonNode = objectMapper.readTree(responseBody);
-
-        Long id = jsonNode.get("id").asLong();
-        String email = jsonNode.get("kakao_account").get("email").asText();
-        String nickname = jsonNode.get("properties")
-                .get("nickname").asText();
-
-        return new KakaoMemberInfo(id, email, nickname);
+        return getResponseBody(response);
     }
 
-    // 3. 카카오ID로 회원가입 처리
-    private Member registerKakaoUserIfNeed(KakaoMemberInfo kakaoMemberInfo, OAuthTokenInfo oAuthTokenInfo) {
-        // DB 에 중복된 email이 있는지 확인
-        // 없으면 닉네임 및 약관 요청 있으면 강제 로그인..
-        // password: random UUID
+    private JsonNode getTokenReissue(OAuth oAuth) throws JsonProcessingException {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_TYPE, "application/x-www-form-urlencoded;charset=utf-8");
 
-        Member member = memberRepository.findByUsername(kakaoMemberInfo.getEmail())
-                .orElseThrow(() -> {
-                    OAuth oAuth = oAuthRepository.save(OAuth.createOAuth(jwtTokenProvider.parseSub(oAuthTokenInfo.getIdToken()),
-                            OAuthType.KAKAO, oAuthTokenInfo.getAccessToken(), oAuthTokenInfo.getRefreshToken()));
-                    throw new SignupNotCompletedException(oAuth.getSub());
-                });
+        // HTTP Body 생성
+        // TODO client_secret 고려해보기
+        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+        body.add("grant_type", "refresh_token");
+        body.add("client_id", CLIENT_ID);
+        body.add("refresh_token", oAuth.getRefreshToken());
 
-        forceLogin(member, oAuthTokenInfo.getRefreshToken());
+        // HTTP 요청 보내기
+        HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest = new HttpEntity<>(body, headers);
+        RestTemplate rt = new RestTemplate();
+        ResponseEntity<String> tokenResponse = rt.exchange(
+                "https://kauth.kakao.com/oauth/token",
+                HttpMethod.POST,
+                kakaoTokenRequest,
+                String.class
+        );
 
-        return member;
+        return getResponseBody(tokenResponse);
     }
 
-    // 4. 강제 로그인 처리
-    private void forceLogin(Member member, String refreshToken) {
-
-        List<GrantedAuthority> authorities = new ArrayList<>();
-        authorities.add(new SimpleGrantedAuthority("MEMBER"));
-
-        UserDetails userDetails = AuthMember.builder()
-                .id(member.getId())
-                .username(member.getUsername())
-                .password(member.getPassword())
-                .authorities(authorities)
-                .build();
-
-        Authentication authentication =
-                new UsernamePasswordAuthenticationToken
-                        (userDetails, "", userDetails.getAuthorities());
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-
-        refreshTokenRepository.save(new RefreshToken(String.valueOf(member.getId()), refreshToken));
+    private JsonNode getResponseBody(ResponseEntity<String> response) throws JsonProcessingException {
+        return new ObjectMapper().readTree(response.getBody());
     }
-
 }
