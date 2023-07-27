537f9942a6a08b6ba3e38906f33b0d9ddc6b3112
hhj <gudwn357@gmail.com>
2023-07-27 15:06:36 +0900

    feat (OAuthRepository) : find by sub

--- src/main/java/kuit/project/beering/repository/OAuthRepository.java
+++ src/main/java/kuit/project/beering/repository/OAuthRepository.java
@@ -3,5 +3,9 @@ package kuit.project.beering.repository;
 import kuit.project.beering.domain.OAuth;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface OAuthRepository extends JpaRepository<OAuth, Long> {
+
+    Optional<OAuth> findBySub(String sub);
 }
