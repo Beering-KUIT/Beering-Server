bbc0442454f10e464af868d491085c7d6dd87269
kwiyoon <jiyun0245@gmail.com>
2023-07-29 03:42:10 +0900

    refactor : resolve N+1 problem by joining favorite
    resolve N+1 problem by joining favorite table when fetching drink list

--- src/main/java/kuit/project/beering/repository/CustomDrinkRepositoryImpl.java
+++ src/main/java/kuit/project/beering/repository/CustomDrinkRepositoryImpl.java
@@ -2,12 +2,14 @@ package kuit.project.beering.repository;
 
 import com.querydsl.core.types.Order;
 import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
-import kuit.project.beering.domain.Drink;
 import kuit.project.beering.dto.request.drink.DrinkSearchCondition;
+import kuit.project.beering.dto.response.drink.DrinkSearchResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -15,9 +17,11 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.util.StringUtils;
 
+import java.util.Collections;
 import java.util.List;
 
 import static kuit.project.beering.domain.QDrink.*;
+import static kuit.project.beering.domain.QFavorite.favorite;
 
 @Slf4j
 public class CustomDrinkRepositoryImpl implements CustomDrinkRepository {
@@ -27,15 +31,28 @@ public class CustomDrinkRepositoryImpl implements CustomDrinkRepository {
         this.jpaQueryFactory = new JPAQueryFactory(em);
     }
 
-    public Page<Drink> search(DrinkSearchCondition condition, Pageable pageable){
-        List<Drink> content = jpaQueryFactory
-                                .selectFrom(drink)
+    public Page<DrinkSearchResponse> search(DrinkSearchCondition condition, Pageable pageable){
+        List<DrinkSearchResponse> content = jpaQueryFactory
+                                .select(Projections.constructor(DrinkSearchResponse.class,
+                                        drink.id, drink.nameKr, drink.nameEn, drink.manufacturer,
+                                        Expressions.constant(Collections.emptyList()), // 우선 빈 리스트
+                                        Expressions.as(
+                                                Expressions.cases()
+                                                        .when(favorite.id.isNotNull()).then(true)
+                                                        .otherwise(false),
+                                                "isLiked"
+                                        )
+                                ))
+                                .from(drink)
                                 .where(eqName(condition.getNameKr(), condition.getNameEn()),
                                         drink.price.between(condition.getMinPrice(), condition.getMaxPrice()),
                                         eqCategory(condition.getCategoryName()))
                                 .orderBy(drinkSort(pageable))
                                 .offset(pageable.getOffset())
                                 .limit(pageable.getPageSize())
+                                .leftJoin(favorite)
+                                .on(drink.id.eq(favorite.drink.id).and(favorite.member.id.eq(condition.getMemberId())))
+                                .fetchJoin()
                                 .fetch();
 
         JPAQuery<Long> countQuery = jpaQueryFactory
