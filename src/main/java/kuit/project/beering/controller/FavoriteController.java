e573e71ce18b5f52b243bc0a05454a246303cc6b
kwiyoon <jiyun0245@gmail.com>
2023-07-30 16:25:45 +0900

    feat : impl POST API for adding drink to favorite
    in FavoriteController

--- src/main/java/kuit/project/beering/controller/FavoriteController.java
+++ src/main/java/kuit/project/beering/controller/FavoriteController.java
@@ -1,8 +1,13 @@
 package kuit.project.beering.controller;
 
+import kuit.project.beering.security.auth.AuthMember;
 import kuit.project.beering.service.FavoriteService;
+import kuit.project.beering.util.BaseResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +15,16 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class FavoriteController {
     private final FavoriteService favoriteService;
+
+    @PostMapping("/members/{memberId}/drinks/{drinkId}/favorites")
+    public BaseResponse<Object> postFavorite(
+            @PathVariable Long memberId,
+            @PathVariable Long drinkId,
+            @AuthenticationPrincipal AuthMember member) {
+
+        favoriteService.addToFavorite(memberId, drinkId);
+
+        return new BaseResponse<>(new Object());
+    }
 
 }
