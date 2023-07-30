5880bdc0610895ca26b2c70c83c6451c777816d7
kwiyoon <jiyun0245@gmail.com>
2023-07-30 16:31:38 +0900

    improve : add validation method for token & member

--- src/main/java/kuit/project/beering/controller/FavoriteController.java
+++ src/main/java/kuit/project/beering/controller/FavoriteController.java
@@ -3,12 +3,17 @@ package kuit.project.beering.controller;
 import kuit.project.beering.security.auth.AuthMember;
 import kuit.project.beering.service.FavoriteService;
 import kuit.project.beering.util.BaseResponse;
+import kuit.project.beering.util.exception.FavoriteException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Objects;
+
+import static kuit.project.beering.util.BaseResponseStatus.TOKEN_PATH_MISMATCH;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,9 +27,15 @@ public class FavoriteController {
             @PathVariable Long drinkId,
             @AuthenticationPrincipal AuthMember member) {
 
+        validateMember(member.getId(), memberId);
         favoriteService.addToFavorite(memberId, drinkId);
 
         return new BaseResponse<>(new Object());
+    }
+
+    private void validateMember(Long authId, Long memberId) {
+        if (!Objects.equals(authId, memberId))
+            throw new FavoriteException(TOKEN_PATH_MISMATCH);
     }
 
 }
