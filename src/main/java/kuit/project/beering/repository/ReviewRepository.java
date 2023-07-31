da516a67f872ea5cb27d1c5f02ce0563f9d7e6de
JooeonLee <cacophony0609@gmail.com>
2023-07-31 12:21:30 +0900

    feat (ReviewRepository) : add findReviewSliceByMemberId query

--- src/main/java/kuit/project/beering/repository/ReviewRepository.java
+++ src/main/java/kuit/project/beering/repository/ReviewRepository.java
@@ -1,8 +1,17 @@
 package kuit.project.beering.repository;
 
 import kuit.project.beering.domain.Review;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.util.List;
 public interface ReviewRepository extends JpaRepository<Review, Long> {
     List<Review> findTop5ByDrinkIdOrderByCreatedAtDesc(Long beerId);
+
+    @Query("select r from Review r " +
+            "where r.member.id = :memberId " +
+            "order by r.createdAt desc ")
+    List<Review> findAllReviewsSliceByMemberIdByCreatedAtDesc(@Param("memberId")Long memberId, Pageable pageable);
 }
