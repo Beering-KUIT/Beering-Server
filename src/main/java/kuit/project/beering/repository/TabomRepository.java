8f9335e8bca618b7471f37ba2628fc2b3e421e5b
JooeonLee <cacophony0609@gmail.com>
2023-07-31 12:23:18 +0900

    feat (TabomRepository) : add count query

--- src/main/java/kuit/project/beering/repository/TabomRepository.java
+++ src/main/java/kuit/project/beering/repository/TabomRepository.java
@@ -1,4 +1,25 @@
 package kuit.project.beering.repository;
 
-public interface TabomRepository {
+import kuit.project.beering.domain.Tabom;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface TabomRepository extends JpaRepository<Tabom, Long> {
+
+    @Query("select count(t) " +
+            "from Tabom t " +
+            "where t.isUp = true " +
+            "and t.review.id = :reviewId ")
+    Optional<Long> findCountByIsUPAndReviewId(@Param("reviewId") Long reviewId);
+
+    @Query("select count(t) " +
+            "from Tabom t " +
+            "where t.isUp = false " +
+            "and t.review.id = :reviewId ")
+    Optional<Long> findCountByIsDownAndReviewId(@Param("reviewId") Long reviewId);
 }
