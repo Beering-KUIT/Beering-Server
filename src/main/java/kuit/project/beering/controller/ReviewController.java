36b669f5a5d718a60ad6f6c2b07953f08e440d10
JooeonLee <cacophony0609@gmail.com>
2023-07-31 12:10:57 +0900

    feat (ReviewController) : add find review by memberId api

--- src/main/java/kuit/project/beering/controller/ReviewController.java
+++ src/main/java/kuit/project/beering/controller/ReviewController.java
@@ -2,12 +2,14 @@ package kuit.project.beering.controller;
 
 import kuit.project.beering.dto.request.review.ReviewCreateRequestDto;
 import kuit.project.beering.dto.response.review.ReviewDetailReadResponseDto;
+import kuit.project.beering.dto.response.review.ReviewReadResponseDto;
 import kuit.project.beering.dto.response.review.ReviewResponseDto;
 import kuit.project.beering.dto.response.reviewOption.ReviewOptionReadResponseDto;
 import kuit.project.beering.service.ReviewService;
 import kuit.project.beering.util.BaseResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -37,5 +39,15 @@ public class ReviewController {
 
         ReviewDetailReadResponseDto responseDto = reviewService.readReviewDetail(reviewId);
         return new BaseResponse<>(responseDto);
+    }
+
+    @GetMapping(value = "/members/{memberId}/reviews")
+    public BaseResponse<List<ReviewReadResponseDto>> readReviewByMemberId(@PathVariable Long memberId,
+                                                                          @RequestParam(value = "page", required = false, defaultValue = "0") int page,
+                                                                          @RequestParam(value = "size", required = false, defaultValue = "4") int size) {
+
+        PageRequest pageRequest = PageRequest.of(page, size);
+        List<ReviewReadResponseDto> responseDtos = reviewService.findAllReviewByMemberIdByPage(memberId, pageRequest);
+        return new BaseResponse<>(responseDtos);
     }
 }
