ffc6b78babcb0a1b77480eca593a9018f54dae0d
kwiyoon <jiyun0245@gmail.com>
2023-08-01 12:26:27 +0900

    refactor (TabomController) : change exception type

--- src/main/java/kuit/project/beering/controller/TabomController.java
+++ src/main/java/kuit/project/beering/controller/TabomController.java
@@ -4,7 +4,7 @@ package kuit.project.beering.controller;
 import kuit.project.beering.security.auth.AuthMember;
 import kuit.project.beering.service.TabomService;
 import kuit.project.beering.util.BaseResponse;
-import kuit.project.beering.util.exception.FavoriteException;
+import kuit.project.beering.util.exception.TabomException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -29,7 +29,7 @@ public class TabomController {
             @PathVariable Long reviewId,
             @RequestParam boolean isUp,
             @AuthenticationPrincipal AuthMember member
-            ){
+    ) {
         validateMember(member.getId(), memberId);
         tabomService.addToTabom(memberId, reviewId, isUp);
         return new BaseResponse<>(new Object());
@@ -37,8 +37,7 @@ public class TabomController {
 
     private void validateMember(Long authId, Long memberId) {
         if (!Objects.equals(authId, memberId))
-            throw new FavoriteException(TOKEN_PATH_MISMATCH);
+            throw new TabomException(TOKEN_PATH_MISMATCH);
     }
-
 
 }
