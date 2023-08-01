bdad46d0796f7c27d1eb1eccb992dee955250d80
kwiyoon <jiyun0245@gmail.com>
2023-08-01 12:34:08 +0900

    feat (exception) : add handler for TypeMismatch

--- src/main/java/kuit/project/beering/controller/advice/GolbalControllerAdvice.java
+++ src/main/java/kuit/project/beering/controller/advice/GolbalControllerAdvice.java
@@ -5,10 +5,14 @@ import kuit.project.beering.util.exception.DrinkException;
 import kuit.project.beering.util.exception.FavoriteException;
 import kuit.project.beering.util.exception.TabomException;
 import lombok.extern.slf4j.Slf4j;
+import net.minidev.json.JSONObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+import static kuit.project.beering.util.BaseResponseStatus.METHOD_ARGUMENT_TYPE_MISMATCH;
 
 @RestControllerAdvice
 @Slf4j
@@ -31,4 +35,12 @@ public class GolbalControllerAdvice {
         return new BaseResponse<>(ex.getStatus());
     }
 
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public BaseResponse<?> handleTypeMismatchException(MethodArgumentTypeMismatchException ex) {
+        // TODO : String으로 반환할지 JSON으로 반환할지 고민
+        // String result = ex.getName() + " : " + ex.getMessage();
+        JSONObject result = new JSONObject();
+        result.put(ex.getName(), ex.getMessage());
+        return new BaseResponse<>(METHOD_ARGUMENT_TYPE_MISMATCH, result);
+    }
 }
