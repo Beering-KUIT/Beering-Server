b5f74ed9ed6a3491411e217d8ef808fe985a7a1a
kwiyoon <jiyun0245@gmail.com>
2023-08-01 12:29:18 +0900

    feat (repository) : add existsByXXId method

--- src/main/java/kuit/project/beering/repository/TabomRepository.java
+++ src/main/java/kuit/project/beering/repository/TabomRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TabomRepository extends JpaRepository<Tabom, Long> {
+    boolean existsByReviewIdAndMemberId(Long memberId, Long reviewId);
 }
