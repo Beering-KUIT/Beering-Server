897fb28617eb5808ef32cd424fb1bca16bf9ea01
kwiyoon <jiyun0245@gmail.com>
2023-08-02 17:25:34 +0900

    feat : impl GET API for getting tabom-up-reviews

--- src/main/java/kuit/project/beering/controller/TabomController.java
+++ src/main/java/kuit/project/beering/controller/TabomController.java
@@ -1,17 +1,18 @@
 package kuit.project.beering.controller;
 
 
+import kuit.project.beering.dto.response.tabom.GetTabomResponse;
+import kuit.project.beering.dto.response.tabom.GetTabomResponsePage;
 import kuit.project.beering.security.auth.AuthMember;
 import kuit.project.beering.service.TabomService;
 import kuit.project.beering.util.BaseResponse;
 import kuit.project.beering.util.exception.TabomException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Objects;
 
@@ -23,7 +24,9 @@ import static kuit.project.beering.util.BaseResponseStatus.TOKEN_PATH_MISMATCH;
 public class TabomController {
     private final TabomService tabomService;
 
-    @PostMapping("/members/{memberId}/reviews/{reviewId}")
+    private final int SIZE = 5;
+
+    @PostMapping("/members/{memberId}/reviews/{reviewId}/tabom")
     public BaseResponse<Object> postTabom(
             @PathVariable Long memberId,
             @PathVariable Long reviewId,
@@ -34,6 +37,23 @@ public class TabomController {
         tabomService.addToTabom(memberId, reviewId, isUp);
         return new BaseResponse<>(new Object());
     }
+
+    @GetMapping("/members/{memberId}/reviews/tabom")
+    public BaseResponse<GetTabomResponsePage> getTabomReview(
+            @PathVariable Long memberId,
+            @RequestParam(required = false, defaultValue = "0") int page,
+            @AuthenticationPrincipal AuthMember member
+    ){
+        validateMember(member.getId(), memberId);
+        Slice<GetTabomResponse> result = tabomService.getTabomReview(memberId, PageRequest.of(page, SIZE));
+
+        return new BaseResponse<>(GetTabomResponsePage.builder()
+                .reviews(result.getContent())
+                .page(result.getNumber())
+                .isLast(result.isLast())
+                .build());
+    }
+
 
     private void validateMember(Long authId, Long memberId) {
         if (!Objects.equals(authId, memberId))
