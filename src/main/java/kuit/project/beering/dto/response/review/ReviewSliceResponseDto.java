07c0f1edc3b8a79026a94ba4fc54761a0df4d75b
JooeonLee <cacophony0609@gmail.com>
2023-08-02 17:03:33 +0900

    feat : add ReviewSliceResponseDto

--- src/main/java/kuit/project/beering/dto/response/review/ReviewSliceResponseDto.java
+++ src/main/java/kuit/project/beering/dto/response/review/ReviewSliceResponseDto.java
@@ -1,5 +1,16 @@
 package kuit.project.beering.dto.response.review;
 
+import lombok.Builder;
+import lombok.Getter;
 
+import java.util.List;
+
+@Getter
+@Builder
 public class ReviewSliceResponseDto {
+
+    List<ReviewReadResponseDto> reviews;
+    private long page;
+    private Boolean isLast;
+
 }
