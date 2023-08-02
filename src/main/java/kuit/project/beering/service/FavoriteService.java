757f20606e66257ad1c3df08da749b06703c6cf0
kwiyoon <jiyun0245@gmail.com>
2023-08-02 18:25:40 +0900

    feat (service) : impl to get favorite drink list

--- src/main/java/kuit/project/beering/service/FavoriteService.java
+++ src/main/java/kuit/project/beering/service/FavoriteService.java
@@ -3,6 +3,7 @@ package kuit.project.beering.service;
 import kuit.project.beering.domain.Drink;
 import kuit.project.beering.domain.Favorite;
 import kuit.project.beering.domain.Member;
+import kuit.project.beering.dto.response.favorite.GetFavoriteDrinkResponse;
 import kuit.project.beering.repository.drink.DrinkRepository;
 import kuit.project.beering.repository.FavoriteRepository;
 import kuit.project.beering.repository.MemberRepository;
@@ -11,8 +12,15 @@ import kuit.project.beering.util.exception.FavoriteException;
 import kuit.project.beering.util.exception.MemberException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static kuit.project.beering.util.BaseResponseStatus.*;
 
@@ -39,4 +47,32 @@ public class FavoriteService {
         favoriteRepository.save(new Favorite(member, drink));
     }
 
+    public Slice<GetFavoriteDrinkResponse> getFavoriteReviews(Long memberId, Pageable pageable) {
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new MemberException(NONE_MEMBER));
+
+        Slice<Drink> drinks = drinkRepository.findByMemberIdAndFavorite(member.getId(), pageable);
+
+        List<GetFavoriteDrinkResponse> dto = drinks.stream()
+                .map(this::getFavoriteDrinkResponseBuild)
+                .collect(Collectors.toList());
+
+        return new SliceImpl<>(dto, pageable, drinks.hasNext());
+    }
+
+    private GetFavoriteDrinkResponse getFavoriteDrinkResponseBuild(Drink drink) {
+        return GetFavoriteDrinkResponse.builder()
+                .drinkId(drink.getId())
+                .nameEn(drink.getNameEn())
+                .nameKr(drink.getNameKr())
+                .manufacturer(drink.getManufacturer())
+                .primaryImageUrl(getPrimaryImage(drink))
+                .build();
+    }
+
+    private String getPrimaryImage(Drink drink) {
+        if(drink.getImages().size() == 0)
+            return null;
+        return drink.getImages().get(0).getImageUrl();
+    }
 }
