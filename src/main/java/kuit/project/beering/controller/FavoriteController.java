729b220a4338b7f337dda9c0f95997c9e8769658
kwiyoon <jiyun0245@gmail.com>
2023-08-02 18:16:08 +0900

    feat : impl GET API for getting favorite drinks

--- src/main/java/kuit/project/beering/controller/FavoriteController.java
+++ src/main/java/kuit/project/beering/controller/FavoriteController.java
@@ -1,15 +1,17 @@
 package kuit.project.beering.controller;
 
+import kuit.project.beering.dto.response.favorite.GetFavoriteDrinkResponse;
+import kuit.project.beering.dto.response.favorite.GetFavoriteDrinkResponsePage;
 import kuit.project.beering.security.auth.AuthMember;
 import kuit.project.beering.service.FavoriteService;
 import kuit.project.beering.util.BaseResponse;
 import kuit.project.beering.util.exception.FavoriteException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Objects;
 
@@ -20,6 +22,7 @@ import static kuit.project.beering.util.BaseResponseStatus.TOKEN_PATH_MISMATCH;
 @Slf4j
 public class FavoriteController {
     private final FavoriteService favoriteService;
+    private final int SIZE = 5;
 
     @PostMapping("/members/{memberId}/drinks/{drinkId}/favorites")
     public BaseResponse<Object> postFavorite(
@@ -31,6 +34,22 @@ public class FavoriteController {
         favoriteService.addToFavorite(memberId, drinkId);
 
         return new BaseResponse<>(new Object());
+    }
+
+    @GetMapping("/members/{memberId}/favorites")
+    public BaseResponse<GetFavoriteDrinkResponsePage> getFavoriteReviews(
+            @PathVariable Long memberId,
+            @RequestParam(required = false, defaultValue = "0") int page,
+            @AuthenticationPrincipal AuthMember member) {
+
+        validateMember(member.getId(), memberId);
+        Slice<GetFavoriteDrinkResponse> result = favoriteService.getFavoriteReviews(memberId, PageRequest.of(page, SIZE));
+
+        return new BaseResponse<>(GetFavoriteDrinkResponsePage.builder()
+                .drinks(result.getContent())
+                .page(result.getNumber())
+                .isLast(result.isLast())
+                .build());
     }
 
     private void validateMember(Long authId, Long memberId) {
