97a8d26a9b4fedc1e5c94bf919e7526abd54d3b3
JooeonLee <cacophony0609@gmail.com>
2023-08-02 17:02:43 +0900

    feat (ReviewRepository) : change response type

--- src/main/java/kuit/project/beering/repository/ReviewRepository.java
+++ src/main/java/kuit/project/beering/repository/ReviewRepository.java
@@ -2,6 +2,7 @@ package kuit.project.beering.repository;
 
 import kuit.project.beering.domain.Review;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -13,5 +14,5 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
     @Query("select r from Review r " +
             "where r.member.id = :memberId " +
             "order by r.createdAt desc ")
-    List<Review> findAllReviewsSliceByMemberIdByCreatedAtDesc(@Param("memberId")Long memberId, Pageable pageable);
+    Slice<Review> findAllReviewsSliceByMemberIdByCreatedAtDesc(@Param("memberId")Long memberId, Pageable pageable);
 }
