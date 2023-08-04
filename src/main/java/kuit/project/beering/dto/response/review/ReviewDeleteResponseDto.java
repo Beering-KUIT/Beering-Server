eec92a08805f788d95205257d13561df2d509e61
JooeonLee <cacophony0609@gmail.com>
2023-08-04 16:23:12 +0900

    feat (ReviewDeleteResponseDto) : add ReviewDeleteResponseDto

--- src/main/java/kuit/project/beering/dto/response/review/ReviewDeleteResponseDto.java
+++ src/main/java/kuit/project/beering/dto/response/review/ReviewDeleteResponseDto.java
@@ -1,4 +1,10 @@
 package kuit.project.beering.dto.response.review;
 
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
 public class ReviewDeleteResponseDto {
+    private Long id;
 }
