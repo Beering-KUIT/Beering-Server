57951e889732967bfcc332a11632c515b967803e
hhj <gudwn357@gmail.com>
2023-08-07 12:35:58 +0900

    feat (OAuthHelperResolver)
    OAuthType 에 따른 적절한 OAuthHelper 반환

---
+++ src/main/java/kuit/project/beering/security/auth/oauth/helper/OAuthHelperResolver.java
@@ -0,0 +1,23 @@
+package kuit.project.beering.security.auth.oauth.helper;
+
+import kuit.project.beering.domain.OAuthType;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class OAuthHelperResolver {
+
+    private final Map<OAuthType, OAuthHelper> oauthHelperMap = new HashMap<>();
+
+    public OAuthHelperResolver(@Qualifier("kakaoOauthHelper") OAuthHelper oauthHelper) {
+        oauthHelperMap.put(OAuthType.KAKAO, oauthHelper);
+    }
+
+    public OAuthHelper getOauthHelper(OAuthType oauthType) {
+        return oauthHelperMap.get(oauthType);
+    }
+
+}
