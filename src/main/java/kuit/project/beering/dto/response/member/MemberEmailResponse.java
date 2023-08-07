3f2e5991e27b87b813570e1a2a1631c9065b5d0d
hhj <gudwn357@gmail.com>
2023-08-07 16:20:01 +0900

    feat : add dtos

---
+++ src/main/java/kuit/project/beering/dto/response/member/MemberEmailResponse.java
@@ -0,0 +1,11 @@
+package kuit.project.beering.dto.response.member;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class MemberEmailResponse {
+
+    private boolean isAvailable;
+}
---
+++ src/main/java/kuit/project/beering/dto/response/member/MemberNicknameResponse.java
@@ -0,0 +1,10 @@
+package kuit.project.beering.dto.response.member;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class MemberNicknameResponse {
+    private boolean isAvailable;
+}
