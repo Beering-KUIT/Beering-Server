7d7ec8b38764d536e8f2733671d2b5651237b518
hhj <gudwn357@gmail.com>
2023-08-07 14:13:36 +0900

    feat (TokenService)
    JwtTokenProviderResolver 의존하도록 로직 수정

--- src/main/java/kuit/project/beering/service/TokenService.java
+++ src/main/java/kuit/project/beering/service/TokenService.java
@@ -7,7 +7,8 @@ import kuit.project.beering.redis.RefreshToken;
 import kuit.project.beering.repository.MemberRepository;
 import kuit.project.beering.repository.RefreshTokenRepository;
 import kuit.project.beering.security.jwt.JwtInfo;
-import kuit.project.beering.security.jwt.jwtTokenProvider.BasicJwtTokenProvider;
+import kuit.project.beering.security.jwt.JwtTokenProviderResolver;
+import kuit.project.beering.security.jwt.jwtTokenProvider.JwtTokenProvider;
 import kuit.project.beering.util.BaseResponseStatus;
 import kuit.project.beering.util.exception.CustomJwtException;
 import lombok.RequiredArgsConstructor;
@@ -23,18 +24,24 @@ public class TokenService {
 
     private final MemberRepository memberRepository;
     private final RefreshTokenRepository refreshTokenRepository;
-    private final BasicJwtTokenProvider basicJwtTokenProvider;
+    private final JwtTokenProviderResolver jwtTokenProviderResolver;
 
     @Transactional(noRollbackFor = CustomJwtException.class)
     public JwtInfo reissueToken(RefreshTokenRequest request) {
 
         String refreshToken = request.getRefreshToken();
 
-        String memberId = validateRefreshToken(refreshToken);
+        JwtTokenProvider jwtTokenProvider = jwtTokenProviderResolver.getProvider(refreshToken);
+
+        String memberId = jwtTokenProvider.validateRefreshToken(refreshToken);
+
+        validateWithRedis(refreshToken, memberId);
 
         // 3. 액세스 토큰, 리프레시 토큰 재발급
-        JwtInfo jwtInfo = basicJwtTokenProvider.createToken(basicJwtTokenProvider.getAuthentication(refreshToken));
-        refreshTokenRepository.save(new RefreshToken(memberId, jwtInfo.getRefreshToken()));
+        JwtInfo jwtInfo = jwtTokenProvider.reissueJwtToken(refreshToken);
+
+        if (jwtInfo.getRefreshToken() != null)
+            refreshTokenRepository.save(new RefreshToken(memberId, jwtInfo.getRefreshToken()));
 
         return jwtInfo;
     }
@@ -42,20 +49,7 @@ public class TokenService {
     /**
      * @Brief 토큰 검증 - 자체 검증(서명, 만료일 등), redis 비교하여 보안 검증 - 탈취 토큰 사용 감지 시 Member 휴먼으로
      */
-    private String validateRefreshToken(String refreshToken) {
-        // 리프레시 토큰 자체 검증
-        try {
-            basicJwtTokenProvider.validateToken(refreshToken);
-        } catch (CustomJwtException ex) {
-            if (ex.getStatus().equals(BaseResponseStatus.EXPIRED_ACCESS_TOKEN)) {
-                throw new CustomJwtException(BaseResponseStatus.EXPIRED_REFRESH_TOKEN);
-            }
-            throw ex;
-        }
-
-
-        // 리프레시 토큰 2차 검증 - redis 와 비교
-        Long memberId = basicJwtTokenProvider.parseMemberId(refreshToken);
+    private String validateWithRedis(String refreshToken, String memberId) {
 
         RefreshToken redisRefreshToken = refreshTokenRepository
                 .findById(String.valueOf(memberId))
@@ -63,10 +57,9 @@ public class TokenService {
 
         if (!refreshToken.equals(redisRefreshToken.getRefreshToken())) {
             // 보안 이슈 발생!! -> 사용자 status 변경
-            Member findMember = memberRepository.findById(memberId).orElseThrow(EntityNotFoundException::new);
+            Member findMember = memberRepository.findById(Long.parseLong(memberId)).orElseThrow(EntityNotFoundException::new);
             findMember.UpdateStatusToDormant();
             memberRepository.save(findMember);
-            System.out.println("findMember.getStatus() = " + findMember.getStatus());
 
             log.error("보안 이슈 발생 memberId = {}", memberId);
             refreshTokenRepository.delete(refreshToken);
