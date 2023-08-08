2529ead1d154e82f4f8305aee8aba927e3dd1d0e
hhj <gudwn357@gmail.com>
2023-08-08 17:02:37 +0900

    refactor (MemberEmailRequest)
    검증 메세지 수정

--- src/main/java/kuit/project/beering/dto/request/member/MemberEmailRequest.java
+++ src/main/java/kuit/project/beering/dto/request/member/MemberEmailRequest.java
@@ -12,6 +12,6 @@ import org.hibernate.validator.constraints.Length;
 public class MemberEmailRequest {
 
     @Email(regexp = "[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}", message = "이메일 형식 확인")
-    @Length(min = 5 ,max = 320, message = "최대 320자리 이내")
+    @Length(min = 5 ,max = 320, message = "5 ~ 320자리 이내")
     private String username;
 }
--- src/main/java/kuit/project/beering/dto/request/member/MemberNicknameRequest.java
+++ src/main/java/kuit/project/beering/dto/request/member/MemberNicknameRequest.java
@@ -11,7 +11,7 @@ import org.hibernate.validator.constraints.Length;
 @Setter
 public class MemberNicknameRequest {
 
-    @Pattern(regexp = "[a-zA-Zㄱ-ㅎㅏ-ㅣ가-힣]+", message = "영문 또는 한글 1 ~ 10자리 이내")
-    @Length(min = 1, max = 10, message = "영문 또는 한글 1 ~ 10자리 이내")
+    @Pattern(regexp = "[a-zA-Zㄱ-ㅎㅏ-ㅣ가-힣]+", message = "영문 또는 한글")
+    @Length(min = 1, max = 10, message = "1 ~ 10자리 이내")
     private String nickname;
 }
