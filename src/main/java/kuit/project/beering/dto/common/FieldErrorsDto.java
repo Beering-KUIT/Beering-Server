5b9b553bd60650e79bdf115909cb056e2cc64327
hhj <gudwn357@gmail.com>
2023-08-09 14:57:04 +0900

    refactor (FieldErrorsDto) : add @Getter

--- src/main/java/kuit/project/beering/dto/common/FieldErrorsDto.java
+++ src/main/java/kuit/project/beering/dto/common/FieldErrorsDto.java
@@ -2,10 +2,12 @@ package kuit.project.beering.dto.common;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 
 import java.util.List;
 
 @AllArgsConstructor
+@Getter
 @Builder
 public class FieldErrorsDto {
 
