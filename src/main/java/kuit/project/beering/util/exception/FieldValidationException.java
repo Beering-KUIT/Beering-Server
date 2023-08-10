96652e1261a6d7c1eb114ac9bc33cbdd0b95dbf2
hamhyeongju <gudwn357@gmai.com>
2023-08-11 00:51:25 +0900

    feat (FieldValidationException) : implement ValidationException

--- src/main/java/kuit/project/beering/util/exception/FieldValidationException.java
+++ src/main/java/kuit/project/beering/util/exception/FieldValidationException.java
@@ -1,16 +1,16 @@
 package kuit.project.beering.util.exception;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
+import kuit.project.beering.util.exception.validation.ValidationException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.validation.BindingResult;
 
-/**
- * @Brief 회원 가입 검증 예외
- */
-@Getter
-@AllArgsConstructor
-public class FieldValidationException extends RuntimeException {
+@Slf4j
+public class FieldValidationException extends ValidationException {
 
-    private BindingResult bindingResult;
-
+    public FieldValidationException(BindingResult bindingResult) {
+        super(bindingResult);
+        log.info("{} - message : {}", this.getClass().getSimpleName(),
+                bindingResult.getAllErrors().stream().map(DefaultMessageSourceResolvable::getDefaultMessage));
+    }
 }
