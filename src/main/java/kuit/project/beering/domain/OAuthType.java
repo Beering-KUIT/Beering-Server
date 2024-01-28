4c8691b18f273594b3dbc13bc9ee9234f1fd2ed8
hhj <gudwn357@gmail.com>
2024-01-28 17:30:54 +0900

    feat (OAuthType) : add value
    value를 통해 issuer 갖도록 함

--- src/main/java/kuit/project/beering/domain/OAuthType.java
+++ src/main/java/kuit/project/beering/domain/OAuthType.java
@@ -1,5 +1,13 @@
 package kuit.project.beering.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public enum OAuthType {
-    KAKAO
+    BEERING("https://beering.com"),
+    KAKAO("https://kauth.kakao.com");
+
+    private final String value;
 }
