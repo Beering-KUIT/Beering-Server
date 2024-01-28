82640b31fc5b5d40c40acac6e4c1dd37179a232b
hhj <gudwn357@gmail.com>
2024-01-28 17:24:28 +0900

    feat (OAuthTypeIssuerMapper) : map issuer and OAuthType

--- src/main/java/kuit/project/beering/security/auth/OAuthTypeIssuerMapper.java
+++ src/main/java/kuit/project/beering/security/auth/OAuthTypeIssuerMapper.java
@@ -1,5 +1,23 @@
 package kuit.project.beering.security.auth;
 
+import kuit.project.beering.domain.OAuthType;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
 public class OAuthTypeIssuerMapper {
+
+    private final Map<String, OAuthType> issuerOAuthTypeMap = new HashMap<>();
+
+    public OAuthTypeIssuerMapper() {
+        issuerOAuthTypeMap.put("https://kauth.kakao.com", OAuthType.KAKAO);
+        issuerOAuthTypeMap.put("https://beering.com", OAuthType.BEERING);
+    }
+
+    public OAuthType get(String issuer) {
+        return issuerOAuthTypeMap.get(issuer);
+    }
 
 }
