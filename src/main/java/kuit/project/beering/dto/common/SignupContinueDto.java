fb80aa5236996c0da0f36b95536829dfe4e86606
hhj <gudwn357@gmail.com>
2024-01-30 16:31:22 +0900

    add (SignupContinueDto)

---
+++ src/main/java/kuit/project/beering/dto/common/SignupContinueDto.java
@@ -0,0 +1,31 @@
+package kuit.project.beering.dto.common;
+
+import kuit.project.beering.domain.OAuthType;
+import kuit.project.beering.dto.request.auth.OAuthSignupRequest;
+import kuit.project.beering.dto.request.member.AgreementRequest;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Builder
+@AllArgsConstructor
+@Getter
+public class SignupContinueDto {
+
+    private OAuthSignupRequest request;
+    private OAuthType oAuthType;
+    private String sub;
+    private String email;
+
+    public String getNickname() {
+        return request.getNickname();
+    }
+    public List<AgreementRequest> getAgreements() {
+        return request.getAgreements();
+    }
+    public String getIdToken() {
+        return request.getIdToken();
+    }
+}
