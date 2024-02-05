d646041cc2a82157b57870cb81532ec54f5a32f1
hhj <gudwn357@gmail.com>
2024-02-05 22:04:41 +0900

    feat (Role) : add GUEST
    prefix : ROLE_ 처리를 위한 코드 추가

--- src/main/java/kuit/project/beering/domain/Role.java
+++ src/main/java/kuit/project/beering/domain/Role.java
@@ -1,5 +1,25 @@
 package kuit.project.beering.domain;
 
 public enum Role {
-    MEMBER, ADMIN
+
+    GUEST("GUEST"),
+    MEMBER("MEMBER"),
+    ADMIN("ADMIN");
+
+    Role(String value) {
+        this.value = value;
+        this.role = PREFIX + value;
+    }
+
+    private final String PREFIX = "ROLE_";
+    private final String value;
+    private final String role;
+
+    public String getValue() {
+        return value;
+    }
+
+    public String getRole() {
+        return role;
+    }
 }
