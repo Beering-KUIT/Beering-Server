b5b15fd3d5fc188c0d65d69d40d065107eeace5b
kwiyoon <43175569+kwiyoon@users.noreply.github.com>
2024-02-10 23:56:18 +0900

    add : create RecordService

---
+++ src/main/java/kuit/project/beering/service/RecordService.java
@@ -0,0 +1,9 @@
+package kuit.project.beering.service;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class RecordService {
+}
