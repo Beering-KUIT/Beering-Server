78edd149130dbf05c41f98249ce84b8605d25e71
kwiyoon <43175569+kwiyoon@users.noreply.github.com>
2024-02-10 23:55:33 +0900

    add : create RecordController

---
+++ src/main/java/kuit/project/beering/controller/RecordController.java
@@ -0,0 +1,11 @@
+package kuit.project.beering.controller;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+public class RecordController {
+
+    }
+}
