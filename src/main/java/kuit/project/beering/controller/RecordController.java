8f12673d38cec619dfdd51342439aa4f740984e6
YoonSub Lim <yslim37@naver.com>
2024-02-14 21:38:31 +0900

    feat: add date validation code and RecordException class

--- src/main/java/kuit/project/beering/controller/RecordController.java
+++ src/main/java/kuit/project/beering/controller/RecordController.java
@@ -1,9 +1,14 @@
 package kuit.project.beering.controller;
 
+import static kuit.project.beering.util.BaseResponseStatus.INVALID_MONTH;
+import static kuit.project.beering.util.BaseResponseStatus.INVALID_YEAR;
+
 import kuit.project.beering.dto.response.record.RecordByDateResponse;
 import kuit.project.beering.security.auth.AuthMember;
 import kuit.project.beering.service.RecordService;
 import kuit.project.beering.util.BaseResponse;
+import kuit.project.beering.util.BaseResponseStatus;
+import kuit.project.beering.util.exception.domain.RecordException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -25,11 +30,20 @@ public class RecordController {
             @RequestParam("year") int year,
             @RequestParam("month") int month,
             @AuthenticationPrincipal AuthMember member) {
-
         log.info("RecordController getUserRecordStatisticByDate 진입 = {} 년 {} 월 조회", year, month);
+
+        validateDate(year, month);
         RecordByDateResponse response = recordService.getUserRecordStatisticByDate(year, month, member.getId());
 
         return new BaseResponse<>(response);
+    }
+
+    private void validateDate(int year, int month) {
+        if (!(2000 <= year && year <= 2999))
+            throw new RecordException(INVALID_YEAR);
+
+        if (!(1 <= month && month <= 12))
+            throw new RecordException(INVALID_MONTH);
     }
 }
 
---
+++ src/main/java/kuit/project/beering/util/exception/domain/RecordException.java
@@ -0,0 +1,15 @@
+package kuit.project.beering.util.exception.domain;
+
+import kuit.project.beering.util.BaseResponseStatus;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
+@Getter
+@Slf4j
+public class RecordException extends DomainException {
+
+    public RecordException(BaseResponseStatus status) {
+        super(status);
+        log.info("{} - message : {}", this.getClass().getSimpleName(), status.getResponseMessage());
+    }
+}
