c45a590e8dbecb13823f8f443eda27b278150e3d
kwiyoon <43175569+kwiyoon@users.noreply.github.com>
2024-02-25 03:53:43 +0900

    refactor : add exception handler for InvalidFormatException

--- src/main/java/kuit/project/beering/controller/advice/FieldValidationControllerAdvice.java
+++ src/main/java/kuit/project/beering/controller/advice/FieldValidationControllerAdvice.java
@@ -1,5 +1,6 @@
 package kuit.project.beering.controller.advice;
 
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import kuit.project.beering.dto.common.FieldErrorsDto;
 import kuit.project.beering.dto.common.FieldValidationError;
 import kuit.project.beering.util.BaseResponse;
@@ -8,6 +9,7 @@ import kuit.project.beering.util.exception.validation.FieldValidationException;
 import lombok.extern.slf4j.Slf4j;
 import net.minidev.json.JSONObject;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -16,14 +18,18 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 
+import static kuit.project.beering.util.BaseResponseStatus.INVALID_FIELD;
 import static kuit.project.beering.util.BaseResponseStatus.METHOD_ARGUMENT_TYPE_MISMATCH;
 
 @Slf4j
 @RestControllerAdvice(annotations = RestController.class)
 public class FieldValidationControllerAdvice {
+
+    private HttpMessageNotReadableException ex;
 
     /**
      * @Brief 필드 검증 오류 시 형식에 맞도록 응답 객체 반환
@@ -31,12 +37,14 @@ public class FieldValidationControllerAdvice {
     @ExceptionHandler(FieldValidationException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public BaseResponse<Object> handleValidationException(FieldValidationException ex) {
+        log.info("handleValidationException");
         return getFieldErrorsDtos(ex.getBindingResult().getFieldErrors());
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public BaseResponse<Object> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex){
+        log.info("handleMethodArgumentNotValidException");
         return getFieldErrorsDtos(ex.getBindingResult().getFieldErrors());
     }
 
@@ -72,5 +80,28 @@ public class FieldValidationControllerAdvice {
         result.put(ex.getName(), msg);
 
         return new BaseResponse<>(METHOD_ARGUMENT_TYPE_MISMATCH, result);
+    }
+
+    @ExceptionHandler(InvalidFormatException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public BaseResponse<?> handleHttpMessageNotReadableException(InvalidFormatException ex) {
+        log.info("InvalidFormatException");
+
+        FieldErrorsDto build = FieldErrorsDto.builder().errors(
+                Collections.singletonList(new FieldValidationError(
+                            extractFieldName(ex.getPath().toString()),
+                            (String) ex.getValue(),
+                            ex.getOriginalMessage()))).build();
+
+        return new BaseResponse<>(INVALID_FIELD, build);
+    }
+
+    private static String extractFieldName(String path) {
+        int startIndex = path.lastIndexOf("[\"");
+        int endIndex = path.lastIndexOf("\"]");
+        if (startIndex != -1 && endIndex != -1) {
+            return path.substring(startIndex + 2, endIndex);
+        }
+        return path;
     }
 }
